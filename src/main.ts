bf9136e7cde044f751f3c1e722aa3278959f8ed3
dbssensei <dimasbagussusilo@gmail.com>
2022-09-24 22:54:20 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap().then((_) => {
+  console.log('Server running on port 3000');
+  return _;
+});
